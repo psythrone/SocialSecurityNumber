a74a5ebb8f262f64f454cc38df767904d227baa8
Mikael Wiik <psythrone@gmail.com>
2020-09-02 08:37:06 +0200

    update program.cs

--- Program.cs
+++ Program.cs
@@ -26,47 +26,19 @@ namespace SocialSecurityNumber
             string birthDateString = socialSecurityNumber.Substring(0, socialSecurityNumber.Length - 5);
 
             bool isFemale = socialSecurityNumber.Length == 11 ? (int.Parse(socialSecurityNumber.Substring(9, 1)) % 2 == 0) : (int.Parse(socialSecurityNumber.Substring(11, 1)) % 2 == 0);
-            //bool isFemale = int.Parse(socialSecurityNumber.Substring(9, 1)) % 2 == 0;
-            string generation = "";
+            
             // Write:[SSN] is a [Age] year old [Sex] ([Verification])
             Console.WriteLine(
-                $"{socialSecurityNumber} is a {CalculateAge(birthDateString, out generation)} year old {(isFemale ? "female" : "male")} belonging to {generation}{(VerifySocialSecurityControlNumber(socialSecurityNumber) ? "" : " and is not a real person")}");
+                $"{socialSecurityNumber} is a {CalculateAge(birthDateString, out string generation)} year old {(isFemale ? "female" : "male")} belonging to {generation}{(VerifySocialSecurityControlNumber(socialSecurityNumber) ? "" : " and is not a real person")}");
         }
 
 
         /// <summary>
-        /// Calculates and checks the verification number on a Swedish social security number
+        /// Calculates a persons current age given their birth date
         /// </summary>
-        /// <param name="socialSecurityNumber">A Swedish Social Security Number with the format (YYMMDD-NNNN)</param>
-        /// <returns>True if Social Security Verification digit is correct</returns>
-        protected static bool VerifySocialSecurityControlNumber(string socialSecurityNumber)
-        {
-            socialSecurityNumber = socialSecurityNumber.Remove(6, 1);
-
-            int accumulation = 0;
-
-            // Calculate verification digit
-            for (int i = 1; i < 10; i++)
-            {
-                int digit = int.Parse(socialSecurityNumber.Substring(i - 1, 1)) * ((i % 2) + 1);
-                if (digit > 9)
-                {
-                    int n = digit % 10;
-                    digit = (digit / 10) + n;
-                }
-                accumulation += digit;
-            }
-            int socialSecturityControlNumber = (10 + ((accumulation / 10) * 10)) - accumulation;
-
-            return socialSecturityControlNumber == int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 1));
-        }
-
-
-        /// <summary>
-        /// Calculates a persons current age given their Swedish Social Security Number
-        /// </summary>
-        /// <param name="socialSecurityNumber">A birth date with the format (yyMMdd) or (yyyyMMdd)</param>
-        /// <returns>The persons current age</returns>
+        /// <param name="socialSecurityNumber">A birth date using format (yyMMdd) or (yyyyMMdd)</param>
+        /// <param name="generation">Out parameter containing the persons generation</param>
+        /// <returns>The persons age</returns>
         protected static int CalculateAge(string socialSecurityNumber, out string generation)
         {
             int age;
@@ -101,8 +73,55 @@ namespace SocialSecurityNumber
 
             return age;
         }
+
+
+        /// <summary>
+        /// Calculates and checks the verification number on a Swedish social security number
+        /// </summary>
+        /// <param name="socialSecurityNumber">A Swedish Social Security Number with the format (YYMMDD-NNNN)</param>
+        /// <returns>True if Social Security Verification digit is correct</returns>
+        protected static bool VerifySocialSecurityControlNumber(string socialSecurityNumber)
+        {
+            //Remove first two digits if format is yyyyMMdd-nnnn, format should be yyMMdd-nnnn
+            if (socialSecurityNumber.Length == 13)
+                socialSecurityNumber = socialSecurityNumber.Substring(2);
+            //Removes the dash for calculation purposes
+            socialSecurityNumber = socialSecurityNumber.Remove(6, 1);
+
+            int accumulation = 0;
+
+            // Calculate verification digit
+            for (int i = 1; i < 10; i++)
+            {
+                int digit = int.Parse(socialSecurityNumber.Substring(i - 1, 1)) * ((i % 2) + 1);
+                if (digit > 9)
+                {
+                    int n = digit % 10;
+                    digit = (digit / 10) + n;
+                }
+                accumulation += digit;
+            }
+            int socialSecturityControlNumber = (10 + ((accumulation / 10) * 10)) - accumulation;
+
+            return socialSecturityControlNumber == int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 1));
+        }
+
     }
 }
+
+/**
+ * Generations:
+ * 
+ * 1910-1924: The Greatest Generation
+ * 1925-1945: The Silent Generation
+ * 1946-1964: Baby Boomer
+ * 1965-1979: Gen X
+ * 1980-1994: Millenial
+ * 1995-2012: Gen Z
+ * 2013-2025: Gen Alpha
+ */
+
+
 
 
 //Console.WriteLine("We come in peace!");
